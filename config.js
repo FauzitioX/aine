4f00feda52360a7e907b13c639c6b815de6c0c5a
FauzitioX <94603698+FauzitioX@users.noreply.github.com>
2022-06-21 02:55:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = FauzyBOT - MD
 global.author = Nomor Bot:6281325984505
-global.wm = '© FauzyBOT - MS'
+global.wm = '© FauzyBOT - MD'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
